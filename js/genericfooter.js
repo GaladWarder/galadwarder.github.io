a4875f0fc6137777d299ba28a8546a74ce84b16d
Grant Roberts <galaddota@yahoo.com>
2023-09-06 14:33:01 -0500

    Breaking out generic footer for easy modularity

---
+++ js/genericfooter.js
@@ -0,0 +1,20 @@
+let appFooter = `
+    <nav>
+       <!-- footer -->
+            <footer class="fixed-footer">
+                <div class="policy-box">
+                    <span>&#169; Grant Roberts Art 2023  /  All rights reserved. </span>
+                </div>
+                <div class="swiper-pagination footer-counter"></div>
+                <div class="footer-social">
+                    <ul>
+                        <li><a href="https://www.artstation.com/grantroberts" target="_blank" ><i class="fa fa-instagram"></i><span>artstation</span></a></li>
+                        <li><a href="https://twitter.com/GaladWarder" target="_blank"><i class="fa fa-twitter"></i><span>twitter / X</span></a></li>
+                        <li><a href="https://www.linkedin.com/in/grantrobertsart/" target="_blank" ><i class="fa fa-pinterest"></i><span>linkedin</span></a></li>
+                    </ul>
+                </div>
+            </footer>
+            <!-- footer end -->
+    </nav>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
